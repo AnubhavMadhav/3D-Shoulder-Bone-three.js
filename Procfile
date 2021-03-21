3de4b4ce9505b753853d84662309d39829217a8f
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2021-03-21 15:55:16 +0530

    Yipee 6

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn assignment.wsgi
--- assignment/settings.py
+++ assignment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3qn2r=)l!3m-2@^7%jb)0i%h4^%mtr=w)uv)0p57-i@^h9+74+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoulder-bone-byanubhav-iiitv.herokuapp.com']
 
 
 # Application definition
